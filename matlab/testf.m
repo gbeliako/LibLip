b6d9d1644b7d7a2674671a4af815f5b4db35bbb6
Gleb Beliakov <gbeliako@gmail.com>
2017-10-18 09:13:42 +1100

    Add files via upload

---
+++ matlab/testf.m
@@ -0,0 +1,3 @@
+function res = testf(x,y)
+% Sample function
+res = exp( -(y - x^2)^2 -  (x^2+y^2)/2);
---
+++ matlab/testf1.m
@@ -0,0 +1,3 @@
+function res = testf1(x,y)
+% Sample function
+res = min(0.8,x*y/(2-x-y+x*y));
